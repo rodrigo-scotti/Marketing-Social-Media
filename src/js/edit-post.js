60189b77e89516aac55d14478ca1ae4d8298b481
rodrigo-scotti <rodrigo.rodrigues@canopusonline.com>
2019-10-07 18:09:37 -0300

    [WIP] Editing post

---
+++ src/js/edit-post.js
@@ -0,0 +1,11 @@
+(function() {
+	let formTitle = document.querySelector('#input-title'),
+		formDescription = document.querySelector('#input-description'),
+		formFile = document.querySelector('#input-file'),
+		formBtn = document.querySelector('#input-button');
+	const postList = JSON.parse(localStorage.getItem('post'));
+
+	const showContent = () => {
+
+	}
+})();
--- src/js/list-post.js
+++ src/js/list-post.js
@@ -2,16 +2,18 @@
 	let tableBody = $('#table-body');
 	const postList = JSON.parse(localStorage.getItem('post'));
 
-	const actionElement = () => {
+	const actionElement = (numId) => {
 		let	td = document.createElement('td'),
 			btnEdit = document.createElement('button'),
 			btnDelete = document.createElement('button');
 
 		btnEdit.setAttribute('type', 'button');
+		btnEdit.setAttribute('id', 'edit' + numId);
 		btnEdit.classList.add('btn', 'btn-secondary', 'btn-sm', 'mr-1');
 		btnEdit.textContent = 'Editar';
 
 		btnDelete.setAttribute('type', 'button');
+		btnDelete.setAttribute('id', 'delete' + numId);
 		btnDelete.classList.add('btn', 'btn-danger', 'btn-sm');
 		btnDelete.textContent = 'Excluir';
 
@@ -34,9 +36,20 @@
 			tableBody[0].appendChild(tr);
 			tr.appendChild(tdDescription);
 			tableBody[0].appendChild(tr);
-			tr.appendChild(actionElement());
+			tr.appendChild(actionElement(key));
 			tableBody[0].appendChild(tr);
 		}
 	};
 	listPosts();
+
+	const redirect = () => {
+		let btnEdit = document.querySelectorAll('button[id^=edit]');
+		btnEdit.forEach(function(item) {
+			item.addEventListener('click', function(){
+				window.location.href = 'edit.html';
+			});
+		});
+	}
+	redirect();
+
 })();
